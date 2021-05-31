aeba504f4f6ce869db1f1759be14a858d960b9cb
Justynr <justynr@me.com>
2021-05-31 17:30:39 -0400

    Add about page - markdown for bio

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,21 @@
+import markdownToHtml from '@/lib/markdownToHtml';
 import Head from 'next/head';
+import About from '../components/About';
+import { getAboutMe } from '../lib/api';
 
-export default function AboutPage() {
+export default function AboutPage({ about, content }) {
   return (
     <div>
       <Head>
         <title>Justyn Roy | About</title>
       </Head>
-      <p>About Page</p>
+      <About about={about} content={content} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const about = await getAboutMe();
+  const content = await markdownToHtml(about?.bio || '');
+  return { props: { about, content } };
 }
