e60d6c2876427606201eb28eec3ac302c3ba7a7d
Justynr <justynr@me.com>
2021-06-07 12:08:52 -0400

    Add load progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/index.css';
+import Nprogress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => Nprogress.start());
+Router.events.on('routeChangeComplete', () => Nprogress.done());
+Router.events.on('routeChangeError', () => Nprogress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
